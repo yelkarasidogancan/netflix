65165b1e78f9d196fdfc621b2375556d4dc2057d
yelkarasidogancan <yelkarasidogancan@gmail.com>
2023-01-04 00:13:51 +0300

    movie_api

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "cd0db94e195ba674cf50c22c0b3eb9c4";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=cd0db94e195ba674cf50c22c0b3eb9c4&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=cd0db94e195ba674cf50c22c0b3eb9c4&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=cd0db94e195ba674cf50c22c0b3eb9c4&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=cd0db94e195ba674cf50c22c0b3eb9c4&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=cd0db94e195ba674cf50c22c0b3eb9c4&language=en-US&page=1`,
+};
+
+export default requests;
